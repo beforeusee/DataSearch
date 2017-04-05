b32d16e03e1f6880d2daa3df94267cb7dafc6ccc
zhengxiaohu <1050087728@qq.com>
2017-04-05 16:45:48 +0800

    1.     将旧版本的MaterialFragment拆分为由两个Fragment组成，分别是MaterialCategories和MaterialDetailFragment，可根据屏幕大小在MaterialFragment中动态的加载Fragment，使得手机和平板能够兼容         1)当使用屏幕较小的手机时，MaterialFragment中仅加载MaterialCategoriesFragment，点击具体的材料选项后，才会动态加载MaterialDetailFragme，当前的MaterialCategoriesFragment进入回退栈，从MaterialDetailFragment返回后才出栈并加载显示。         2)当使用屏幕大的平板时，MaterialFragment中动态加载MaterialCategoriesFragment和MaterialDetailFragment显示在同一个fragment_material中。         点击MaterialCategoriesFragment    的“材料管理”进入MaterialCategoriesManagementActivity。 2.    完成材料管理的MaterialCategoriesManagementActivity的布局activity_material_categories_management.xml和添加材料分类及删除材料分类的事务处理，成功连接了数据库，可对材料的分类进行管理，添加或者删除分类。       1)布局设计          A：创建自定义的标题栏样式及注册监听函数，点击“完成”可返回。          B：创建自定义的添加材料的AddBarLayout，点击添加材料分类。          C：为RecyclerView创建自定义的ItemView样式，并绑定到ViewHolder.       2)RecyclerView的ItemView的点击事件创建          在RecyclerView的Adapter即MaterialCategoriesAdapter中利用ViewHolder获取itemView布局，采用观察者模式，为itemView设置点击的Listener并对外提供了setOnItemListener方法，RecyclerView不能直接设置点击事件监听函数，必须借助MaterialCategoriesAdapter的对象adapter来对RecyclerView的itemView进行点击事件的注册，并写相应的逻辑。 3.    在ToolFragment的DestroyView中添加了广播的注销，从而修复了之前没有注销广播的bug。广播的注册和注销必须在同一对生命周期中。如在onCreateView中注册，则应在onDestroyView注销。

--- app/src/main/java/com/example/a103/datasearch/MaterialCategoriesAdapter.java
+++ app/src/main/java/com/example/a103/datasearch/MaterialCategoriesAdapter.java
@@ -1,15 +1,13 @@
 package com.example.a103.datasearch;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.example.a103.datasearch.data.MaterialCategories;
-
 import java.util.List;
 
 
@@ -18,42 +16,72 @@ import java.util.List;
  */
 
 public class MaterialCategoriesAdapter extends
-        RecyclerView.Adapter<MaterialCategoriesAdapter.ViewHolder> {
+        RecyclerView.Adapter<MaterialCategoriesAdapter.ViewHolder>{
 
+    private static final String TAG = "MaterialCategoriesAdapter";
     private List<MaterialCategories> materialCategoriesList;
-    static class ViewHolder extends RecyclerView.ViewHolder{
+    private OnRecyclerViewItemClickListener mOnItemClickListener;
+
+    static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         ImageView materialCategoriesDeleteView;
         TextView materialCategoriesName;
-        public ViewHolder(View itemView) {
+        private OnRecyclerViewItemClickListener mOnItemClickListener=null; //声明接口变量
+
+        public ViewHolder(View itemView,OnRecyclerViewItemClickListener listener) {
             super(itemView);
             materialCategoriesName= (TextView) itemView.
                     findViewById(R.id.tv_material_categories_name);
             materialCategoriesDeleteView= (ImageView) itemView.findViewById(R.id.iv_delete_categories);
+
+            this.mOnItemClickListener=listener;
+//            itemView.setOnClickListener(this);    //点击模式：设置RecyclerView中整个列表项的监听回调函数
+            materialCategoriesDeleteView.setOnClickListener(this); //点击模式：设置RecyclerView中列表项的删除按钮监听回调函数
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (mOnItemClickListener!=null){
+                mOnItemClickListener.onItemClick(v,getAdapterPosition());
+                Log.d(TAG, "onClick: 静态类ViewHolder中点击Item的adapterPosition位置: "+getAdapterPosition());
+            }
         }
     }
 
-    public MaterialCategoriesAdapter(List<MaterialCategories> materialCategoriesList){
+    MaterialCategoriesAdapter(List<MaterialCategories> materialCategoriesList){
         //通过构造函数传入适配器的数据源
         this.materialCategoriesList=materialCategoriesList;
+    }
+
+    void updateMaterialCategoriesList(List<MaterialCategories> list){
+        if (materialCategoriesList!=null){
+            materialCategoriesList.clear();
+            materialCategoriesList.addAll(list);
+        }else {
+            materialCategoriesList=list;
+        }
+        notifyDataSetChanged();
+
+    }
+
+    //观察者模式——定义接口OnRecyclerViewItemClickListener为抽象观察者，
+    // 实现该接口的MaterialCategoriesManagementActivity为具体的观察者，
+    // 而观察的具体主题是MaterialCategoriesAdapter的对象adapter中ViewHolder中的根布局itemView，其抽象主题是View
+    interface OnRecyclerViewItemClickListener{
+        void onItemClick(View view,int position);
+    }
+
+    //观察者模式——对MaterialCategoriesAdapter的对象提供一个注册的方法
+    void setOnItemClickListener(OnRecyclerViewItemClickListener itemClickListener) {
+        this.mOnItemClickListener = itemClickListener;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView= LayoutInflater.from(parent.getContext()).
                 inflate(R.layout.material_categories_item,parent,false);
-        final ViewHolder holder=new ViewHolder(itemView);
-        //点击删除按钮的监听函数
-        holder.materialCategoriesDeleteView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int position=holder.getAdapterPosition();
-                MaterialCategories materialCategories=materialCategoriesList.get(position);
-                Toast.makeText(v.getContext(),"点击了删除!",Toast.LENGTH_SHORT).show();
-            }
-        });
 
-        return holder;
+        return new ViewHolder(itemView,mOnItemClickListener);
     }
 
     @Override
--- app/src/main/java/com/example/a103/datasearch/MaterialCategoriesManagementActivity.java
+++ app/src/main/java/com/example/a103/datasearch/MaterialCategoriesManagementActivity.java
@@ -1,13 +1,25 @@
 package com.example.a103.datasearch;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.a103.datasearch.dao.DaoSession;
 import com.example.a103.datasearch.data.MaterialCategories;
@@ -23,7 +35,10 @@ public class MaterialCategoriesManagementActivity extends AppCompatActivity {
     RecyclerView mRecyclerView;
     LinearLayout mAddBarLinearLayout;
     private List<MaterialCategories> materialCategoriesList=new ArrayList<>();
-    DaoSession daoSession= DatabaseApplication.getDaoSession();  //获取应用的全局数据库管理变量daoSession
+    private DaoSession daoSession= DatabaseApplication.getDaoSession();  //获取应用的全局数据库管理变量daoSession
+    private MaterialCategoriesAdapter adapter;
+
+    private static final String TAG = "MaterialCategoriesManagementActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +56,7 @@ public class MaterialCategoriesManagementActivity extends AppCompatActivity {
         mRecyclerView.setLayoutManager(layoutManager);
 
         //创建并设置mRecyclerView的适配器
-        MaterialCategoriesAdapter adapter=new MaterialCategoriesAdapter(materialCategoriesList);
+        adapter=new MaterialCategoriesAdapter(materialCategoriesList);
         mRecyclerView.setAdapter(adapter);
 
         //设置mAddBarLinearLayout的监听回调函数
@@ -49,11 +64,124 @@ public class MaterialCategoriesManagementActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // TODO: 2017/3/28 弹出添加材料分类的对话框
+                final Dialog dialog=new Dialog(MaterialCategoriesManagementActivity.this);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.add_material_categories_dialog);
+
+                //添加材料分类对话框的控件
+                final EditText editMaterialCategories= (EditText) dialog.findViewById(R.id.et_add_material_categories);
+                final Button categoriesCommitButton= (Button) dialog.findViewById(R.id.add_material_categories_button_commit);
+                final Button categoriesCancelButton= (Button) dialog.findViewById(R.id.add_material_categories_button_cancel);
+
+                //初始化categoriesCommitButton不可用
+                categoriesCommitButton.setEnabled(false);
+
+                //设置EditText的监听器，根据用户是否输入有效字符串来判断categoriesCommitButton是否可点击，
+                // 禁止用户不做任何输入，从而在数据库中创建空名称的(无意义)材料分类数据
+                editMaterialCategories.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                    }
+
+                    /**
+                     * @param s
+                     * tag s为变化后的所有字符
+                     */
+                    @Override
+                    public void afterTextChanged(Editable s) {
+                        if ("".equals(s.toString().trim())){
+                            categoriesCommitButton.setEnabled(false); //如果去掉所有空白字符的s为空，则设置commit按钮不可点击
+                        }else {
+                            categoriesCommitButton.setEnabled(true);  //s不为空，可点击
+                        }
+                    }
+                });
+
+                //设置commitButton的监听器
+                categoriesCommitButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //创建MaterialCategories对象
+                        MaterialCategories materialCategories=new MaterialCategories();
+                        materialCategories.setName(editMaterialCategories.getText().toString());
+
+                        //存入数据库
+                        daoSession.getMaterialCategoriesDao().save(materialCategories);
+
+                        //如果数据库中存在刚保存的数据则创建成功并更新材料列表
+                        if (daoSession.getMaterialCategoriesDao().hasKey(materialCategories)){
+                            Log.d(TAG, "categoriesCommitButton:onClick: successfully saved: "+materialCategories.getName());
+                            //更新材料分类列表
+                            updateMaterialCategories();
+                        }else {
+                            Log.d(TAG, "categoriesCommitButton:onClick: failed to save: "+materialCategories.getName()+
+                            " in the database table MATERIAL_CATEGORIES");
+                        }
+
+                        //销毁对话框
+                        dialog.dismiss();
+                    }
+                });
+
+                //设置categoriesCancelButton的监听器
+                categoriesCancelButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();  //销毁添加材料的对话框
+                    }
+                });
+
+                //显示添加材料的对话框
+                dialog.show();
+                Log.d(TAG, "mAddBarLinearLayout:onClick: 添加材料分类的对话框顺利弹出");
             }
         });
 
-        // TODO: 2017/3/28 RecyclerView的点击回调函数注册,暂时没想到好办法，先在适配器类中处理
+        //RecyclerView的点击事件：采用了设计模式的中的观察者模式
+        //利用adapter设置列表单击事件，监听接口自定义设置在MaterialCategoriesAdapter中
+        adapter.setOnItemClickListener(new MaterialCategoriesAdapter.OnRecyclerViewItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                final MaterialCategories materialCategories=materialCategoriesList.get(position);
+                Log.d(TAG, "onItemClick: click materialCategories: "+materialCategories.getName());
 
+                //弹出删除的警告对话框
+                AlertDialog.Builder alertDialogBuilder=new AlertDialog.Builder(MaterialCategoriesManagementActivity.this);
+                alertDialogBuilder.setTitle("提示");
+                alertDialogBuilder.setMessage("确定删除该材料分类吗？");
+                alertDialogBuilder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //点击“确定”，删除所选刀具
+                        //判断数据表中是否有materialCategories类型的数据，有则删除并更新materialCategoriesList列表
+                        // 如果没有打印调试信息表示不存在该数据
+                        if (daoSession.getMaterialCategoriesDao().hasKey(materialCategories)){
+                            daoSession.getMaterialCategoriesDao().delete(materialCategories);
+                            updateMaterialCategories();
+                            Log.d(TAG, "onItemClick: successfully deleted: "+materialCategories.getName());
+                        }else{
+                            Log.d(TAG, "onItemClick: failed to delete: "+materialCategories.getName()+
+                                    ",the data does not exist in the database table MATERIAL_CATEGORIES");
+                        }
+                    }
+                });
+
+                alertDialogBuilder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                AlertDialog alertDialog=alertDialogBuilder.create();
+                alertDialog.show();
+            }
+        });
 
         //设置右边按钮“完成”的监听函数
         mCustomTitleBar.setTitleBarRightBtnClickListener(new View.OnClickListener() {
@@ -62,13 +190,22 @@ public class MaterialCategoriesManagementActivity extends AppCompatActivity {
                 // TODO: 2017/3/28 点击,finish该对话框,并负责刷新fragment_material的材料分类列表
                 finish();
 
-                //刷新fragment_material的业务逻辑
+                //TODO 刷新fragment_material的分类列表的业务逻辑
             }
         });
+
+        Log.d(TAG, "onCreate: successfully execution!");
     }
 
     private void initialMaterialCategories() {
         materialCategoriesList=daoSession.getMaterialCategoriesDao().loadAll();
+        Log.d(TAG, "initialMaterialCategories: 加载数据库中材料分类列表的数据库");
+    }
+
+    private void updateMaterialCategories(){
+        materialCategoriesList=daoSession.getMaterialCategoriesDao().loadAll();
+        adapter.updateMaterialCategoriesList(materialCategoriesList);
+        Log.d(TAG, "updateMaterialCategories: 执行材料分类列表更新");
     }
 
     private void initialMaterialCategoriesManagementView() {
@@ -79,7 +216,7 @@ public class MaterialCategoriesManagementActivity extends AppCompatActivity {
     }
 
     /**
-     * 该类为外界启动该活动提供了启动的方法和需要传入的参数，有时可能利用intent传入参数
+     * 该activity类为外界提供了启动本activity的方法和需要传入的参数，有时可能利用intent传入参数
      * @param context
      */
     public static void actionStart(Context context){
